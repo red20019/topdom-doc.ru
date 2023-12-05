42a28c6541a9f2f503064c795de287727a53bc3c
Albert <ari1004@rambler.ru>
2023-12-05 14:47:08 +0300

    'neInsaf'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ALO INSAF GDE SERVER
+                    ALO INSAF GDE SERVER S GITOM
                 </div>
 
                 <div id="hello-react"></div>
