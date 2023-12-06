3fc425b9f4af57f5d0903af9926c79cb4d0cf976
JediKnightGIT <jediknightru@gmail.com>
2023-12-06 16:35:00 +0300

    add tailwindcss #2

---
+++ resources/js/Application.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export interface IUser {
+    name: string;
+    age: number;
+}
+const App = () => {
+    const [users, setUsers] = useState<IUser[]>([
+        {
+            name: "Bijaya",
+            age: 25,
+        },
+        {
+            name: "Ram",
+            age: 25,
+        },
+    ]);
+
+    return (
+        <div>
+            <h1 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Users list</h1>
+            <ul className="max-w-md space-y-1 text-gray-500 list-disc dark:text-gray-400">
+                {users.map((user: IUser) => {
+                    return (
+                        <li key={user.name}>
+                            {user.name} is {user.age} years old
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.ts",
+    "./resources/**/*.tsx",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
