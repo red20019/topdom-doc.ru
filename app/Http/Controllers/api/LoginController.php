62cf3fa549ef453aee2ab8ea0704606056e1ab59
Albert <ari1004@rambler.ru>
2024-01-19 13:05:24 +0300

    logout add

--- app/Http/Controllers/api/LoginController.php
+++ app/Http/Controllers/api/LoginController.php
@@ -16,24 +16,30 @@ class LoginController extends Controller
   {
 
 
-  //$validator = Validator::make($request->all(), [
-  //  'email' => 'required|email',
-  //  'password' => 'required',
-  //]);
+    //$validator = Validator::make($request->all(), [
+    //  'email' => 'required|email',
+    //  'password' => 'required',
+    //]);
 
-  $user = User::where('email', $request->email)->first();
+    $user = User::where('email', $request->email)->first();
 
-  if (! $user || ! Hash::check($request->password, $user->password)) {
+    if (! $user || ! Hash::check($request->password, $user->password)) {
 
-      return $this->sendError('Logining Error.', 'error');
-      //throw ValidationException::withMessages([
-      //    'email' => ['The provided credentials are incorrect.'],
-      //]);
-  }
-  $success['token'] =  $user->createToken($request->email)->plainTextToken;
-  $success['name'] =  $user->name;
-  return $this->sendResponse($success, 'User logining.');
+        return $this->sendError('Logining Error.', 'error');
+        //throw ValidationException::withMessages([
+        //    'email' => ['The provided credentials are incorrect.'],
+        //]);
+    }
+    $success['token'] =  $user->createToken($request->email)->plainTextToken;
+    $success['name'] =  $user->name;
+    return $this->sendResponse($success, 'User logining.');
   //$user->createToken($input['email'])->accessToken;
+  }
 
+  public function logout(Request $request)
+  {
+    $request->user()->currentAccessToken()->delete();
+    $success['name'] =  $request->user()->name;
+    return $this->sendResponse($success, 'User logout.');
   }
 }
