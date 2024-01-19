b6975811e8d43144ef68a12ab97acf033140b1fb
Jedi Knight <jediknightru@gmail.com>
2024-01-19 11:59:04 +0300

    password confirmation fix

--- resources/js/pages/SignUp.tsx
+++ resources/js/pages/SignUp.tsx
@@ -96,14 +96,14 @@ const SignUp: React.FC = () => {
           <div className="mb-6">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password-confirmation"
+              htmlFor="password_confirmation"
             >
               Подтвердите пароль
             </label>
             <input
               onChange={handleChange}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password-confirmation"
+              id="password_confirmation"
               type="password"
               placeholder="********"
             />
