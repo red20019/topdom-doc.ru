60f0de24e5bc01544658fcf37963ac0a2874277e
Albert <ari1004@rambler.ru>
2024-02-19 22:53:02 +0300

    Create document output

--- database/migrations/2024_02_08_161521_create_document_trackings_table.php
+++ database/migrations/2024_02_08_161521_create_document_trackings_table.php
@@ -16,7 +16,7 @@ class CreateDocumentTrackingsTable extends Migration
         Schema::create('document_trackings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('document_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->string('stage_document');
+            $table->integer('stage_document');
             $table->datetime('date_start_stage');
             $table->datetime('date_end_stage')->nullable();
             $table->timestamps();
